daf2606e481f35f7c891bb023bd68a320df6b8f1
Yash Kelkar <yashnkelkar@gmail.com>
2026-08-07 22:17:54 -0400

    add base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import babel from '@rolldown/plugin-babel'
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    base: '/',
   ],
 })
